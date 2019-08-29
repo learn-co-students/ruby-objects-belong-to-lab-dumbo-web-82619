9ebda06c41689878031cb566033dd903aa94fecb
Franky Liu <cuifranky95@gmail.com>
2019-08-29 12:57:33 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,12 @@
+class Artist
+
+# def initialize(name)
+#   @name = name
+# end
+
+def name=(new_name)
+  @name = new_name
+end
+  attr_reader :name
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,8 @@
+class Song
+
+attr_accessor :title, :artist
+
+def title=(new_title)
+  @title = new_title
+end
+end
